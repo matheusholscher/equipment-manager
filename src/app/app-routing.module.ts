ee9b6a1e2d6fdedb72cfca7880e1743b2a174363
matheusholscher <matheusholscher@gmail.com>
2019-11-27 21:50:42 -0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { EquipmentsComponent } from './equipments/equipments.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent, children : [
+        { path: 'equipments', component: EquipmentsComponent },
+        { path: 'reservation', component: ReservationsComponent }
+    ]},
+    { path: '', pathMatch: 'full', redirectTo: '/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { EquipmentsComponent } from './equipments/equipments.component';
+import { ReservationsComponent } from './reservations/reservations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    EquipmentsComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule,
